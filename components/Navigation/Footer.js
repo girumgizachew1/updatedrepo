6ce2b17075d08f31680781d090f6fa94f7fdc1a2
unknown <giri@10000>
2023-01-17 13:26:08 +0300

    nextjs performance optimization

---
+++ components/Navigation/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer className="footer">
+<img
+  src="svg/text.png"
+  alt="design and devloped by Girum.G"
+/>
+<div className="footer__socials">
+         <a
+    href="https://github.com/girumgizachew1"
+    target="_blank"
+    rel="noopener"
+  >
+    <img src="svg/github.svg" alt="github logo" />
+  </a>
+</div>
+</footer>
+    </div>
+  )
+}
+
+export default Footer
